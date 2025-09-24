1f2b42f08887f4a331c7131f604d85939e5e83e7
Arthur <197347033+OnlyOneArthur@users.noreply.github.com>
2025-09-24 20:32:02 +0800

    adding spotify to dotfiles

--- home.nix
+++ home.nix
@@ -54,6 +54,7 @@
   home.file = {
      ".config/nvim".source = ./dotfiles/nvim;
      ".config/kitty".source = ./dotfiles/kitty;
+     ".config/spotify".source = ./dotfiles/spotify;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
