6f343d1fab9fe310f217dabc8bad58547c8abbdd
Arthur <197347033+OnlyOneArthur@users.noreply.github.com>
2025-10-01 05:44:56 +0800

    fixing the error from earlier

--- flake.nix
+++ flake.nix
@@ -22,8 +22,12 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
-        modules = [ ./customScript.nix];
+        modules = [ 
+        ./home.nix 
+        ./customScript.nix
+
+
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
